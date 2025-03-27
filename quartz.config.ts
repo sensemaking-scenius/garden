e73bebbb600ff761c29787b4e19321875fb1f68a
Clinamenic <ssc@clinamenic.com>
2025-03-27 07:59:23 -0700

    re-enable RSS feed now that submodule fetching is fixed

--- quartz.config.ts
+++ quartz.config.ts
@@ -84,7 +84,7 @@ const config: QuartzConfig = {
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
-        enableRSS: false,
+        enableRSS: true,
       }),
       Plugin.Assets(),
       Plugin.Static(),
